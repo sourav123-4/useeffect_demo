88335eac5386506668b9704104f88aff20c19b32
sourav12 <souravmahanty12@hmail.com>
2022-04-04 18:15:42 +0530

    useeffect done

--- src/App.js
+++ src/App.js
@@ -1,23 +1,30 @@
-import logo from './logo.svg';
-import './App.css';
-
+import React from 'react'
+import './App.css'
 function App() {
+  const [name,setName]=React.useState("posts")
+  const [item,setItem]=React.useState([])
+  const [windowwidth,setWindowwidth]=React.useState(window.innerWidth)
+  function handleclick(){
+    setWindowwidth(window.innerWidth)
+  }
+  React.useEffect(()=>{
+    window.addEventListener('resize',handleclick)
+  },[])
+  React.useEffect(()=>{
+    fetch(`https://jsonplaceholder.typicode.com/${name}`)
+    .then(res=>res.json())
+    .then(json=>setItem(json))
+  },[name])
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <p>window size: {windowwidth}</p>
+      <button onClick={()=>{setName('posts')}}>Posts</button>
+      <button onClick={()=>{setName('users')}}>Users</button>
+      <button onClick={()=>{setName('comments')}}>Comments</button>
+      <h1>{name}</h1>
+      {item.map((data)=>{
+        return <p>{JSON.stringify(data)}</p>
+      })}
     </div>
   );
 }
